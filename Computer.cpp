aa934e2916cb4c6d64c929edcf8c65586e55be17
Quentin Lurkin <qlurkin@gmail.com>
2018-10-04 15:42:31 +0200

    Add Compute Shader

---
+++ Computer.cpp
@@ -0,0 +1,51 @@
+#include "Computer.h"
+#include "Application.h"
+
+#include <iostream>
+
+using namespace std;
+
+Computer::Computer(GLuint compute_shader)
+{
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 0, &workgroup_count[0]);
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 1, &workgroup_count[1]);
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, 2, &workgroup_count[2]);
+
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 0, &workgroup_size[0]);
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 1, &workgroup_size[1]);
+    glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &workgroup_size[2]);
+
+    glGetIntegerv (GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &workgroup_invocations);
+
+    program = glCreateProgram();
+	glAttachShader(program, compute_shader);
+	glLinkProgram(program);
+
+	GLint status;
+	glGetProgramiv (program, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+		
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+
+		cout << "Program link error: " << strInfoLog;
+		delete[] strInfoLog;
+	}
+}
+
+void Computer::setData(int index, GLuint data)
+{
+    glUseProgram(program);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, index, data;
+    glUseProgram(NULL);
+}
+
+void Computer::compute(int count_x, int count_y, int count_z)
+{
+    glUseProgram(program);
+    glDispatchCompute(count_x, count_y, count_z);
+    glUseProgram(NULL);
+}
---
+++ Computer.h
@@ -0,0 +1,25 @@
+#ifndef COMPUTER_H
+#define COMPUTER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "vmath.h"
+
+using namespace vmath;
+
+class Computer
+{
+    private:
+    GLuint program;
+    int workgroup_count[3];
+    int workgroup_size[3];
+    int workgroup_invocations;
+
+    public:
+    Program(GLuint compute_shader);
+    void setData(int index, GLuint data);
+    void compute(int count_x, int count_y, int count_z);
+};
+
+#endif
